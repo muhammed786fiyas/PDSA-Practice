3b4bce308a3e86f8671673baaabd1ade40b63e06
Muhammed Fiyas <muhammed786fiyas@gmail.com>
2025-08-06 12:01:54 +0530

    Create sort an array of 0's ,1's and 2's.py

---
+++ ARRAYS/sort an array of 0's ,1's and 2's.py
@@ -0,0 +1,23 @@
+
+def sortColors(nums):
+    """
+    Do not return anything, modify nums in-place instead.
+    """
+    low=0
+    mid=0
+    high=len(nums)-1
+
+    while(mid<=high):
+        if nums[mid]==0:
+            nums[mid],nums[low]=nums[low],nums[mid]
+            low+=1
+            mid+=1
+        elif nums[mid]==1:
+            mid+=1
+        else:
+            nums[mid],nums[high]=nums[high],nums[mid]
+            high-=1
+    return nums
+
+nums=[0,1,2,2,1,2,0,0,0,1,2,2,2,1,1]
+print(sortColors(nums))
