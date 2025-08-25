4a793cdd12dce99ab05a42e52af08e870356d7c6
Muhammed Fiyas <muhammed786fiyas@gmail.com>
2025-08-25 08:08:48 +0530

    Create 1 finding duplicate number in array.py

---
+++ ARRAYS/1 finding duplicate number in array.py
@@ -0,0 +1,18 @@
+def findDuplicate(nums):
+    # Good problem uses linked list cycle method
+    slow=nums[0]
+    fast=nums[0]
+    while(1):
+        slow=nums[slow]
+        fast=nums[nums[fast]]
+        if(slow==fast):
+            break
+    fast=nums[0]
+    while(slow!=fast):
+        slow=nums[slow]
+        fast=nums[fast]
+    
+    return slow
+
+nums=[1,3,3,4,5,2]
+print(findDuplicate(nums))
